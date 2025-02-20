cabe6f7000b43774ff41bb67a3345c8a52596e9d
Ilnar-912 <sadretdinovilnar17@gmail.com>
2025-02-20 18:25:15 +0300

    исправлен код в main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:comicsstudio/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'core/app.dart';
 
-void main() {
+void main() async {
+  await AppRouter.initialize();
   runApp(const App());
 }
